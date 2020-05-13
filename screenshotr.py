d818f1a8c5947b2783f356823fdae5e64450e1ce
Colm Britton <colmjude@gmail.com>
2020-05-13 22:36:10 +0100

    Fullpage screenshots or default size

--- screenshotr.py
+++ screenshotr.py
@@ -40,9 +40,6 @@ def add_url_to_img(url, screenshot):
 
     chosen_font = ImageFont.truetype(os.path.join(system_fonts_foler, font_name), 19)
     size = draw.textsize(txt_str, chosen_font)
-    #height = size[1] + (2*margin)
-    #width = size[0] + (2*margin)
-    #print(size, [width, height])
     container = add_text_container(draw, img.size, size)
     
     draw.text((container[0]+20,container[1]+20), txt_str, fill='#0b0c0c', font=chosen_font)
@@ -50,7 +47,7 @@ def add_url_to_img(url, screenshot):
     img.save(screenshot)
 
 
-async def screenshotr(url, output):
+async def screenshotr(url, output, fullPage=True):
     browser = None
     page = None
     if browser is None:
@@ -66,12 +63,25 @@ async def screenshotr(url, output):
         os.mkdir("screenshots")
 
     await page.goto(url)
+    # default setting
+    # sets the page size for non fullpage screenshots
+    await page.setViewport({"width":1200,"height":1600})
     await page.emulateMedia('screen')
-    await page.screenshot({
+
+    opts = {
         'path': screenshot_path,
         'type': 'png',
-        'fullPage': True
-    })
+        'fullPage': fullPage
+    }
+    # if not fullPage:
+    #     opts['clip'] = {
+    #         'x': 0,
+    #         'y': 0,
+    #         'width': 1000,
+    #         'height': 1600
+    #     }
+
+    await page.screenshot(opts)
 
     await browser.close()
     add_url_to_img(url, screenshot_path)
@@ -80,7 +90,11 @@ async def screenshotr(url, output):
 if __name__ == "__main__":
     args_provided = sys.argv[1:]
     print(args_provided)
-    if len(args_provided) == 2:
-        asyncio.get_event_loop().run_until_complete(screenshotr(url=args_provided[0], output=args_provided[1]))
+    if len(args_provided) >= 2:
+        fullPage = True
+        if len(args_provided) > 2:
+            if args_provided[2] and args_provided[2] in ['F', 'f', 'False', 'false']:
+                fullPage = False
+        asyncio.get_event_loop().run_until_complete(screenshotr(url=args_provided[0], output=args_provided[1], fullPage=fullPage))
     else:
         print("No URL provided")
